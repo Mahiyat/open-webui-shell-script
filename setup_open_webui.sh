5990e3d594cc2154443f458ae61bfd81f6953556
Mahiyat <mahiyat.tanzim@gmail.com>
2025-01-28 23:48:09 +0600

    Add docker compose file for stable-diffusion and open-webui containers
    Modify the shell script file
    
    Pull more models
    
    Update README.md

--- setup_open_webui.sh
+++ setup_open_webui.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Define variables for the container and image names
-CONTAINER_NAME="open-webui"
-IMAGE_NAME="ghcr.io/open-webui/open-webui:ollama"
-OLLAMA_VOLUME="ollama"
-WEBUI_VOLUME="open-webui"
+# Define variables for the container name
+CONTAINER_NAME="open-webui-custom"
 
 # Function to check if a command was successful
 check_success() {
@@ -14,15 +11,12 @@ check_success() {
   fi
 }
 
-echo "Starting the setup for Open WebUI with Ollama..."
+echo "Starting the setup for Open WebUI with Ollama, and Stable Diffusion..."
 
 # Run the Docker container
-echo "Running the Docker container..."
-docker run -d -p 3000:8080 --gpus=all \
-  -v $OLLAMA_VOLUME:/root/.ollama \
-  -v $WEBUI_VOLUME:/app/backend/data \
-  --name $CONTAINER_NAME --restart always $IMAGE_NAME
-check_success "Failed to run the Docker container."
+echo "Running the Docker containers..."
+docker compose up -d --build
+check_success "Failed to run the Docker containers."
 
 # Pull the llama3.2 model
 echo "Pulling the llama3.2 model..."
@@ -33,5 +27,21 @@ check_success "Failed to pull the llama3.2 model."
 echo "Pulling the llava model..."
 docker exec -it $CONTAINER_NAME ollama pull llava
 check_success "Failed to pull the llava model."
+
+# Pull the deepseek-r1:7b model
+echo "Pulling the deepseek-r1:7b model..."
+docker exec -it $CONTAINER_NAME ollama pull deepseek-r1:7b
+
+# Pull the stable diffusion model
+echo "Pulling the stable diffusion model..."
+docker exec -it $CONTAINER_NAME ollama pull brxce/stable-diffusion-prompt-generator
+
+# Pull the codellama model
+echo "Pulling the codellama model..."
+docker exec -it $CONTAINER_NAME ollama pull codellama
+
+# Pull the deepseek-r1:7b model
+echo "Pulling the deepseek-r1:8b model..."
+docker exec -it $CONTAINER_NAME ollama pull deepseek-r1:8b
 
 echo "Setup completed successfully!"
